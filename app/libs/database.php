93632ddee25dbb4e122c94a6b3384baf46d9efbd
jostinaguilar <aguilarjostin2001@gmail.com>
2022-10-22 21:41:59 -0500

    fix connection linux

--- app/libs/database.php
+++ app/libs/database.php
@@ -4,7 +4,7 @@ class Database
 {
   public function connect()
   {
-    $con = new mysqli("127.0.0.1", "root", "123abc", "todoapp");
+    $con = new mysqli("127.0.0.1", "root", "mysql@LINUX22", "todoapp");
     if ($con->connect_errno) {
       $messageError =  "Fallo al conectar a MySQL: ({$con->connect_errno}) {$con->connect_error}";
       return $messageError;
